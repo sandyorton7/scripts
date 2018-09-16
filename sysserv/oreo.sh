878dace692c0afb1d03e37255b430970269a620e
sandyorton7 <sandyorton7@gmail.com>
2018-09-16 17:35:02 +0000

    syyserv is lub

---
+++ sysserv/oreo.sh
@@ -0,0 +1,101 @@
+<html>
+<head>
+<meta charset="utf-8" />
+<title>dogbin - sysservoreo</title>
+<link rel="shortcut icon" href="/static/favicon.ico" />
+<link rel="preload" href="/static/fonts/hack-regular.woff2?sha=3114f1256" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="/static/fonts/hack-bold.woff2?sha=3114f1256" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="/static/fonts/hack-italic.woff2?sha=3114f1256" as="font" type="font/woff2" crossorigin>
+<link rel="stylesheet" type="text/css" href="/static/hack.css">
+<link rel="stylesheet" type="text/css" href="/static/app.css">
+<meta property="og:title" content="dogbin - sysservoreo" />
+<meta property="og:site_name" content="dogbin" />
+<meta name="keywords" content="Pastebin,URL Shortener,code,log,deletescape">
+<meta name="theme-color" content="#37474f">
+<meta name="description" content="#!/bin/bash
+
+# sysserv
+repopick --gerrit https://substratum.review -P frameworks/base/ 423 424 425 4">
+<meta property="og:description" content="#!/bin/bash
+
+# sysserv
+repopick --gerrit https://substratum.review -P frameworks/base/ 423 424 425 4" />
+<meta property="og:image" content="https://del.dog/doggo.jpg" />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-54490986-2"></script>
+<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-54490986-2');
+		</script>
+</head>
+<body>
+<ul id="messages" class="unselectable"></ul>
+<div id="header" class="unselectable">
+<div id="title"><a href="/">dogbin</a></div>
+<div id="tabs">
+<a class="tab " href="/">PASTE</a>
+<a class="tab " href="/about.md">ABOUT</a>
+<a class="tab " href="/changelog.md">CHANGELOG</a>
+</div>
+<div id="slug">
+<a id="url_display" href="https://del.dog/sysservoreo">/sysservoreo</a>
+<button class="copy_url btn enabled" tabindex="2"><img src="/static/icons/copy.svg" alt="Copy URL"></button>
+</div>
+<div id="actions">
+<button class="save action " tabindex="3"><img src="/static/icons/save.svg" alt="Save"></button>
+<button class="edit action " tabindex="3"><img src="/static/icons/edit.svg" alt="Edit"></button>
+<button class="new action enabled" tabindex="3"><img src="/static/icons/create.svg" alt="New"></button>
+<button class="duplicate action  enabled " tabindex="3"><img src="/static/icons/copy.svg" alt="Duplicate &amp; Edit"></button>
+<button class="raw action  enabled " tabindex="3"><img src="/static/icons/raw.svg" alt="Just Text"></button>
+<button class="twitter action  enabled " tabindex="3"><img src="/static/icons/twitter.svg" alt="Twitter"></button>
+</div>
+</div>
+<div id="content">
+<div id="linenos" class="unselectable">
+<a href="#L1" id="L1">1</a><br />
+<a href="#L2" id="L2">2</a><br />
+<a href="#L3" id="L3">3</a><br />
+<a href="#L4" id="L4">4</a><br />
+<a href="#L5" id="L5">5</a><br />
+<a href="#L6" id="L6">6</a><br />
+<a href="#L7" id="L7">7</a><br />
+<a href="#L8" id="L8">8</a><br />
+<a href="#L9" id="L9">9</a><br />
+<a href="#L10" id="L10">10</a><br />
+<a href="#L11" id="L11">11</a><br />
+</div>
+<pre id="box" tabindex="1"><code class="hljs ">#!/bin/bash
+
+# sysserv
+repopick --gerrit https://substratum.review -P frameworks/base/ 423 424 425 427 430 431 448 454 458
+repopick --gerrit https://substratum.review -P packages/apps/Settings/ 447
+repopick --gerrit https://substratum.review -P system/sepolicy 432 450
+repopick --gerrit https://substratum.review -P build/make/ 459
+
+# exposures
+repopick --gerrit https://substratum.review -P frameworks/base/ 46{6..9} 46{0..5} 470 47{5..8} 480 455 481
+repopick --gerrit https://substratum.review -P packages/apps/Settings/ 471 472 473 479 482</code></pre>
+</div>
+<div id="footer" class="unselectable">
+<div id="copyright">Copyright &copy; 2018 <a href="https://deletescape.ch" target="_blank" rel="noopener">deletescape</a></div>
+<div id="author">
+Created by Anonymous
+</div>
+<div id="stats">Stats:&nbsp;&nbsp;
+<div id="views"><img src="/static/icons/eye.svg" alt="Views"><b>2</b></div>
+</div>
+</div>
+</body>
+<link rel="stylesheet" type="text/css" href="/static/hljs-dogbin.css" />
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script src="/static/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="/static/application.js"></script>
+<script type="text/javascript">
+	// Construct app
+	$(function () {
+		var app = new haste();
+	});
+</script>
+</html>
